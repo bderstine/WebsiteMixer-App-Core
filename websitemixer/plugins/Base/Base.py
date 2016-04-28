99ce8b2020dd95449a370460e57e0b3ffa109581
Brad Derstine <brad@bizzartech.com>
2016-04-28 06:37:03 -0400

    move Base to Core

--- websitemixer/plugins/Base/Base.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, Response, session, request, url_for, redirect, render_template, abort, g, send_from_directory
-from websitemixer import app
-
-@app.route('/')
-def home():
-    return render_template("Base/index.html")
